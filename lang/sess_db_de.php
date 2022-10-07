7bce099b438824b7809573bf1100549035389705
gizmore <gizmore@wechall.net>
2022-10-07 17:29:30 +0200

    '7.0.1-r1654'

---
+++ lang/sess_db_de.php
@@ -0,0 +1,7 @@
+<?php
+namespace GDO\Session\lang;
+return [
+'module_session' => 'Sitzung (DB)','info_privacy_session_db' => 'Dieses Modul speichert Ihre Sitzung in der Datenbankund setzt ein Cookie, um Sie wiederzuerkennen.<br/>
+Ein Cookie und eine Sitzung sind erforderlich, um diesen Dienst zu betreiben.<br/>
+Dieses Cookie wird <b>*nicht*</b> mit anderen Websites geteilt.',
+];
